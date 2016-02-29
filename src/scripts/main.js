a9efb44c7e312ca2356a304bf678e55c734a46cd
Matt Gerton <matt.gerton@gmail.com>
2016-02-29 17:59:08 -0600

    Optimize background image. Update main page with two-button layout instead of typing text.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,35 @@
 // var malarkey = require('malarkey');
 
 function main() {
-  var roles = document.querySelector('.role');
-  var options = {
-    typeSpeed: 65,
-    deleteSpeed: 65,
-    pauseDelay: 2000,
-    loop: true,
-    postfix: '.'
-  };
+  // var roles = document.querySelector('.role');
+  // var options = {
+  //   typeSpeed: 65,
+  //   deleteSpeed: 65,
+  //   pauseDelay: 2000,
+  //   loop: true,
+  //   postfix: '.'
+  // };
 
-  malarkey(roles, options)
-    .type('web developer').pause().delete()
-    .type('beer geek').pause().delete()
-    .type('dreamer').pause().delete()
-    .type('problem solver').pause().delete()
-    .type('comic nerd').pause().delete()
-    .type('music lover').pause().delete()
-    .type('typography nerd').pause().delete()
-    .type('sports fan').pause().delete();
+  // malarkey(roles, options)
+  //   .type('web developer').pause().delete()
+  //   .type('beer geek').pause().delete()
+  //   .type('dreamer').pause().delete()
+  //   .type('problem solver').pause().delete()
+  //   .type('comic nerd').pause().delete()
+  //   .type('music lover').pause().delete()
+  //   .type('typography nerd').pause().delete()
+  //   .type('sports fan').pause().delete();
+
+  // var slideout = new Slideout({
+  //   'panel': document.querySelector('.home'),
+  //   'menu': document.querySelector('.slide-panel'),
+  //   'padding': 256,
+  //   'tolerance': 70
+  // });
+
+  // document.querySelector('.btn-about').addEventListener('click', function() {
+  //   slideout.toggle();
+  // });
 }
 
 window.onload = main;
