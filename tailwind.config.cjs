2fc6d0d07920029b4ac26a7a926cd368944e4b4d
Matt Gerton <matt.gerton@gmail.com>
2024-08-07 00:02:29 -0700

    feat(style): add basic theme config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'transparent': 'transparent',
+			'current-color': 'currentColor',
+			brand: {
+				primary: '#0C1A9C',
+				secondary: '#03A1D3',
+				white: '#F7F5FB',
+				gray: '#E1E3F3',
+			},
+			content: {
+				primary: '#090E3C',
+			}
+		},
 		extend: {},
 	},
 	plugins: [],
